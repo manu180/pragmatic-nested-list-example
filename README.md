5cd84d63cf98ff81abb21da3eb6954a0b0a0bd8b
schmima <manuel.schmitt@curia.europa.eu>
2025-11-25 17:22:17 +0100

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Pragmatic nested list
 
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/~/github.com/manu180/pragmatic-nested-list-example)
+
 An example on how to handle drag'n'drop with *nested lists* using [Pragmatic drag and drop](https://github.com/atlassian/pragmatic-drag-and-drop) and leverages the [list-item hitbox](https://atlassian.design/components/pragmatic-drag-and-drop/optional-packages/hitbox/about#list-item).
 
 This project is powered by [Vite](https://vitejs.dev/).
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import type { Group } from './types/data';
-import { getData } from './data/data';
-import List from './components/list';
+import type { Group } from "./types/data";
+import { getData } from "./data/data";
+import List from "./components/list";
 
 function App() {
   const items: Group[] = getData();
   return (
-    <div className="max-w-7xl mx-auto p-8 bg-white min-h-screen">
-      <h1 className="text-xl text-gray-800 text-center mb-8">
-        pragmatic-drag-and-drop - Nested Lists
-      </h1>
+    <div className="py-10 w-80 bg-white min-h-screen">
       <List items={items} />
     </div>
   );
